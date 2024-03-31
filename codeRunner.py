d50e1d657d170bd20b4a2aa221f6a463b71edaf7
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2024-03-31 14:26:28 +0530

    final

--- codeRunner.py
+++
@@ -1,3 +0,0 @@
- statergy = Statergy(data);
-    accuracy, returns, trades = statergy.macd_strategy()
-    print("\033[1;32mAccuracy: {:.2f}% Returns: {:.2f}% Trades: {}\033[0m".format(accuracy * 100, returns, trades))
